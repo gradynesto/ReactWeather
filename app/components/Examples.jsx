1eabf31d463132f0552640ad91cbf07316894483
gradynesto <ernie.uribe@gmail.com>
2018-02-10 16:37:39 -0500

    update middleware for localhost

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
+        <div>
         <h3>Examples Component!</h3>
         <p>welcome to examples page!</p>
-    )
+        </div>
+    );
 };
 
 module.exports = Examples;
