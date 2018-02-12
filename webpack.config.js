0f689574fae744e39803685c7198cd0541977ce7
gradynesto <ernie.uribe@gmail.com>
2018-02-12 17:47:26 -0500

    add Foundation & jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -30,5 +45,5 @@ module.exports = {
       }
     ]
   }
-  ,devtool: 'cheap-module-eval-source-map'
+  , devtool: 'cheap-module-eval-source-map'
 };
