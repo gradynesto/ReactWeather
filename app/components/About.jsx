f1f4ddb5d06da5bfc2d7fa56fbf4ae0092f12284
gradynesto <ernie.uribe@gmail.com>
2018-02-12 17:22:58 -0500

    updating about page with welcome text

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About =  (props) => {
     return (
+        <div>
         <h3>About Component</h3>
+        <p>Welcome to our about page!</p>
+        </div>
     )
 }
 
