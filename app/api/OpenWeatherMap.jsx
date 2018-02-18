ccefaddaebd0ddad3fb1123cf7615a161f83f4a3
gradynesto <ernie.uribe@gmail.com>
2018-02-18 12:28:19 -0500

    Add error modal

--- app/api/OpenWeatherMap.jsx
+++ app/api/OpenWeatherMap.jsx
@@ -14,7 +14,7 @@ module.exports = {
                 return res.data.main.temp;
             }
         }, function (err) {
-            throw new Error(err.response.data.message);
+            throw new Error('Unable to fetch weather for that location.');
         });
 
     }
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
-      OpenWeatherMap: 'app/api/OpenWeatherMap.jsx'
+      OpenWeatherMap: 'app/api/OpenWeatherMap.jsx',
+      ErrorModal: 'app/components/ErrorModal.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
